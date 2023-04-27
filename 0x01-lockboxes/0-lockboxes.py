696d9af1b77cd46f1b7ef7ce4ac3a0f0182c22ad
trefania <tarirotrefania@gmail.com>
2023-04-27 23:48:48 +0200

    unlockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,12 @@
+def canUnlockAll(boxes):
+    # list of unlocked boxes
+    unlocked_boxes = [0]
+
+    # iterate over unlocked boxes and add keys to new boxes
+    for box in unlocked_boxes:
+        for key in boxes[box]:
+            if key not in unlocked_boxes:
+                unlocked_boxes.append(key)
+
+    # if all boxes are unlocked, return True
+    return len(unlocked_boxes) == len(boxes)
