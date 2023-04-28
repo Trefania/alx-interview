4f155ad34abe822895f3cdbe1af7e97986374d38
trefania <tarirotrefania@gmail.com>
2023-04-28 12:55:59 +0200

    unlock the box

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,11 +5,11 @@ def canUnlockAll(boxes):
     Determines if all the boxes can be opened.
 
     Args:
-    boxes: A list of lists, where each inner list represents a box and
-           contains the keys to other boxes.
+        boxes: A list of lists, where each inner list represents a box and
+               contains the keys to other boxes.
 
     Returns:
-    True if all boxes can be opened, else False.
+        True if all boxes can be opened, else False.
     """
     num_boxes = len(boxes)
     # create a list to keep track of which boxes are unlocked
@@ -17,18 +17,17 @@ def canUnlockAll(boxes):
     # the first box is unlocked by default
     unlocked[0] = True
     # create a list to keep track of the keys we have
-    keys = [0]
+    keys = boxes[0]
     # loop through the keys we have and try to unlock new boxes
     while keys:
         # get the first key from the list
         key = keys.pop(0)
-        # loop through the boxes the key can unlock
-        for box in boxes[key]:
-            # check if the box is already unlocked
-            if not unlocked[box]:
-                # unlock the box
-                unlocked[box] = True
-                # add the keys from the box to the list
-                keys.extend(boxes[box])
+        # check if we already unlocked this box
+        if unlocked[key]:
+            continue
+        # unlock the box
+        unlocked[key] = True
+        # add the keys from the box to the list
+        keys.extend(boxes[key])
     # check if all boxes are unlocked
     return all(unlocked)
