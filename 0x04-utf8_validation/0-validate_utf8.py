42d747f019f9d60b08447e7dd485d7bd682136ab
Trefania <tarirotrefania@gmail.com>
2023-05-18 17:57:04 +0000

    validated utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,10 +5,11 @@ a method that determines if a given data set represents a valid UTF-8 encoding.
 """
 
 def validUTF8(data):
-""" a valid utf-8 encoding """
-    num_bytes =0:
+# Nums of bytes currently in e UTF-8 char
 
-    for byte in data:
+ num_bytes =0
+
+for byte in data:
         # Check if the byte is a continuation byte
         if num_bytes == 0:
             if (byte >> 5) == 0b110:
