629e947fd0321290cafd6402d618edb1e9fa4b4e
trefania <tarirotrefania@gmail.com>
2023-05-18 21:27:17 +0200

    changes in utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,6 +4,7 @@
 a method that determines if a given data set represents a valid UTF-8 encoding.
 """
 
+
 def validUTF8(data):
     """Determines if a given data set
     represents a valid utf-8 encoding
