24ad382037758afd7c6e97b9be85ee1f83348b11
trefania <tarirotrefania@gmail.com>
2023-06-01 17:09:43 +0200

    star wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,36 +2,22 @@
 
 const request = require('request');
 
-// Function to retrieve the character names from a specific Star Wars movie
-function getMovieCharacters(movieId) {
-  const url = `https://swapi.dev/api/films/${movieId}/`;
-
-  // Make a request to the Star Wars API to get the movie data
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-    } else {
-      const film = JSON.parse(body);
-      const characters = film.characters;
-
-      // Iterate over the character URLs and fetch their data
-      characters.forEach((characterUrl) => {
-        // Make a request to each character URL
-        request(characterUrl, (error, response, body) => {
-          if (error) {
-            console.error('Error:', error);
-          } else {
-            const character = JSON.parse(body);
-            console.log(character.name); // Print the character name
-          }
-        });
-      });
-    }
-  });
-}
-
-// Retrieve the movie ID from the command line argument
 const movieId = process.argv[2];
 
-// Call the function with the provided movie ID
-getMovieCharacters(movieId);
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, async (error, response, body) => {
+  error && console.log(error);
+
+  const charactersArray = (JSON.parse(response.body).characters);
+  for (const character of charactersArray) {
+    await new Promise((resolve, reject) => {
+      request(character, (error, response, body) => {
+        err && console.log(error);
+
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
+});
