9971deabc9cefba8d3452ad3b7f65eb99a1f1d93
trefania <tarirotrefania@gmail.com>
2023-06-18 21:02:11 +0200

    making changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-""""
-Main file for testing
-"""
+"""A function to determine the fewest number of coins needed
+   to meet a given amount total"""
+
 
 def makeChange(coins, total):
-    """ If the total is 0 or less, return 0 """
+    """This function calculates the minimum number of coins needed
+       to reach a specified total amount
+    """
     if total <= 0:
         return 0
-
-    # Initialize a list to store the fewest number of coins needed for each amount
-    min_coins = [float('inf')] * (total + 1)
-    # The minimum number of coins needed to make 0 is 0
-    min_coins[0] = 0
-
-    # Iterate over each coin value
-    for coin in coins:
-        # Iterate over each amount from the coin value to the total
-        for amount in range(coin, total + 1):
-            # Update the minimum number of coins needed for the current amount
-            # by taking the minimum of the current minimum and the minimum for the
-            # remaining amount after subtracting the current coin value plus 1
-            min_coins[amount] = min(min_coins[amount], min_coins[amount - coin] + 1)
-
-    # If the minimum number of coins for the total is still infinity, it means
-    # the total cannot be met by any combination of coins
-    if min_coins[total] == float('inf'):
+    else:
+        # Sort the coins in descending order
+        coin = sorted(coins, reverse=True)
+        counter = 0
+        # Iterate through the coins and subtract them from the total
+        for i in coin:
+            while total >= i:
+                counter += 1
+                total -= i
+        # If the total amount is reduced to zero, return the counter
+        if total == 0:
+            return counter
+        # If the total amount cannot be reached with the given coins, return -1
         return -1
-
-    # Return the minimum number of coins needed for the total
-    return min_coins[total]
