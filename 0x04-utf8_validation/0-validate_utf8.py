e2861f0b335782a576bdffe63cc861922f8422c7
trefania <tarirotrefania@gmail.com>
2023-07-21 20:54:53 +0200

    validate

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
-
-"""
-a method that determines if a given data set represents a valid UTF-8 encoding.
-"""
+""" UTF-8 Validation """
 
 
 def validUTF8(data):
-    """Determines if a given data set
-    represents a valid utf-8 encoding
     """
-    num_bytes = 0
+    Method that determines if a given data set represents a valid
+    UTF-8 encoding.
+    """
+    number_bytes = 0
 
-    for byte in data:
-        # Check if the byte is a continuation bytes
-        if num_bytes == 0:
-            if (byte >> 5) == 0b110:
-                num_bytes = 1
-            elif (byte >> 4) == 0b1110:
-                num_bytes = 2
-            elif (byte >> 3) == 0b11110:
-                num_bytes = 3
-                # If the byte doesn't match any UTF-8 encoding pattern
-            elif (byte >> 7) != 0:
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
                 return False
+
         else:
-            # Check if the byte is a continuation byte
-            if (byte >> 6) != 0b10:
-                return False
-            # Checking  if there is any  remaining bytes that don't form a complete UTF-8 char
-            num_bytes -= 1
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
 
-    return num_bytes == 0
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
